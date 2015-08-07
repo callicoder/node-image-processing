799c9808b0ae31dcc3d311e6c64decbdd79e5564
Rajeev Kumar Singh <callicoder@gmail.com>
2015-08-07 09:16:42 +0530

    chat settings

--- public/modules/chat/chat.client.controller.js
+++ public/modules/chat/chat.client.controller.js
@@ -7,9 +7,12 @@ angular.module('materialApp')
 	$scope.messages = [];
 
 	$scope.sendMessage = function() {
-		console.log($scope.message);
-    	Socket.emit('chatMessage', $scope.message);
-    	$scope.message = '';
+		console.log($scope.messageText);
+		var message = {
+			text: $scope.messageText
+		};
+    	Socket.emit('chatMessage', message);
+    	$scope.messageText = '';
 	};
 
 	Socket.on('chatMessage', function(msg){
